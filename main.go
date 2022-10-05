b33bd5f89a9d368d53c7635c8790d0ba114a8dbe
Hilful <hilful25@gmail.com>
2022-10-05 20:41:01 +0600

    First commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Ashabul Elaph Hilful"
+
+	// println creates a new line after execution
+	fmt.Println(name)
+	fmt.Println("Welcome !!")
+
+	// Simply prints without  going to new line
+	fmt.Printf("Text in same line!!!")
+	fmt.Printf("SEE!!!")
+
+	var message, message2 = "Wanna know a Secret??", "Let's do it"
+	fmt.Printf("Let me tell you: ow wait %v,%v ,Still trying ?", message, message2)
+
+	fmt.Println()
+
+	var value int
+
+	fmt.Scan(&value)
+	fmt.Println(`Your typed number is :`, value)
+
+	fmt.Scanln(&value)
+	fmt.Println("Your second value is :", value)
+
+	fmt.Scanf("%d", &value) //formated scan
+	fmt.Println("Your last value is :", value)
+}
